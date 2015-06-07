4c0da89edaa582a25657bdfc3f04d9bef335326f
Monte Mishkin <montemishkin@gmail.com>
2015-06-07 16:19:50 -0700

    temporary working solution with homebrew, python, python3, virtualenvwrapper

--- bash/bash_profile.sh
+++ bash/bash_profile.sh
@@ -1,6 +1,5 @@
 # monte's custom stuff
 source ${HOME}/mdv/dotfiles/bash/config/path.sh
 source ${HOME}/mdv/dotfiles/bash/config/aliases.sh
-source ${HOME}/mdv/dotfiles/bash/config/functions.sh
 source ${HOME}/mdv/dotfiles/bash/config/bindkeys.sh
 source ${HOME}/mdv/dotfiles/bash/config/prompt.sh
--- bash/config/path.sh
+++ bash/config/path.sh
@@ -1,15 +1,20 @@
-# Setting PATH for Python 3.4
-export PATH="/Library/Frameworks/Python.framework/Versions/3.4/bin:${PATH}"
-
-# MacPorts Installer addition on 2015-02-07_at_23:42:24: 
-#   adding an appropriate PATH variable for use with MacPorts.
-export PATH="/opt/local/bin:/opt/local/sbin:/Users/montemishkin/tools/bin:$PATH"
-
-# virtualenvwrapper settings
-source /opt/local/Library/Frameworks/Python.framework/Versions/3.4/bin/virtualenvwrapper.sh
-
-# local node bin
+# prepend local node bin
 export PATH="$HOME/.node/bin:$PATH"
 
 # prepend monte's personal bin
 export PATH="$HOME/bin:$PATH"
+
+
+
+
+# virtualenvwrapper settings
+export WORKON_HOME=$HOME/.virtualenvs
+export VIRTUALENVWRAPPER_PYTHON=/usr/local/bin/python3
+# export PROJECT_HOME=$HOME/mdv
+source /usr/local/bin/virtualenvwrapper.sh
+
+
+# homebrew bash completion
+source /usr/local/Library/Contributions/brew_bash_completion.sh
+
+export EDITOR=vim
