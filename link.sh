556f47f392a1799b35eacfa6580eb796ffea8c64
Monte Mishkin <montemishkin@gmail.com>
2015-06-08 21:06:10 -0700

    fixed link.sh bugs from spaces in path names

--- link.sh
+++ link.sh
@@ -10,53 +10,51 @@
 
 
 # if an argument was passed to this script
-if [ ! -z $1 ]; then
+if [ ! -z "$1" ]; then
     # then use it as the dir to save old config files to
-    save_dir=$1
+    save_dir="$1"
 # no argument passed to script
 else
     # use default timestamped path as dir to save old config files to
-    save_dir=$HOME/.config.$(date +%Y-%m-%d_%H.%M.%S)
+    save_dir="$HOME/config.$(date +%Y-%m-%d_%H.%M.%S)"
 fi
 
 
 # first argument is path where config file is expected to be
 # second argument is path where config file actually is
-copy_and_link() {
+save_then_link() {
     # if $1 is a directory
-    if [ -d $1 ]; then
+    if [ -d "$1" ]; then
         echo "$1 is a directory (no action taken)"
     # otherwise, $1 does not exist or is not a directory
     else
         # if $1 does exist but is not a directory
-        if [ -a $1 ]; then
+        if [ -a "$1" ]; then
             # if $save_dir has not been made yet
-            if [ ! -d save_dir ]; then
-                echo "creating directory $save_dir to save old copies of config files to"
+            if [ ! -d "$save_dir" ]; then
+                echo "making directory $save_dir to save old copies of config files to"
                 # make dir to save old config files to
-                mkdir -p $save_dir
+                mkdir -p "$save_dir"
             fi
 
-            echo "saving old copy of $1 in $save_dir"
+            echo "saving old copy of $1"
             # copy old config file to dir of saved config files
-            cp -R -L $1 $save_dir
+            cp -R -L "$1" "$save_dir"
             # remove old config file from original location
-            rm $1
+            rm "$1"
         fi
 
         echo "creating link: $1 -> $2"
         # create link to new config file
-        ln -s $2 $1
+        ln -s "$2" "$1"
     fi
 }
 
 
-dotfiles=$HOME/mdv/dotfiles
+dotfiles="$HOME/mdv/dotfiles"
 
-copy_and_link $HOME/.bash_boi $dotfiles/bash/bash_profile.sh
-
-# copy_and_link $HOME/.bash_profile $dotfiles/bash/bash_profile.sh
-# copy_and_link $HOME/.gitconfig $dotfiles/git/gitconfig
-# copy_and_link $HOME/.npmrc $dotfiles/npm/npmrc
-# copy_and_link $HOME/.vimrc $dotfiles/vim/vimrc
-# copy_and_link $HOME/Library/Application Support/Sublime Text 2/Packages/User/Preferences.sublime-settings $dotfiles/sublime/Preferences.sublime-settings
+save_then_link "$HOME/.bash_profile" "$dotfiles/bash/bash_profile.sh"
+save_then_link "$HOME/.gitconfig" "$dotfiles/git/gitconfig"
+save_then_link "$HOME/.npmrc" "$dotfiles/npm/npmrc"
+save_then_link "$HOME/.vimrc" "$dotfiles/vim/vimrc"
+save_then_link "$HOME/Library/Application Support/Sublime Text 2/Packages/User/Preferences.sublime-settings" "$dotfiles/sublime/Preferences.sublime-settings"
