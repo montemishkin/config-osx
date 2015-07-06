123d63a73c0f479e814afdbec08e79dbc692848f
Monte Mishkin <montemishkin@gmail.com>
2015-07-06 11:42:27 -0700

    bash now sources all bash completion files installed by homebrew

--- bash/config/completion.sh
+++ bash/config/completion.sh
@@ -3,3 +3,6 @@ source /usr/local/Library/Contributions/brew_bash_completion.sh
 
 # gulp tab completion
 eval "$(gulp --completion=bash)"
+
+# various tab completions (npm, git, etc) installed by homebrew
+source /usr/local/etc/bash_completion.d/*
