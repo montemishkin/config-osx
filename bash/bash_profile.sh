5c37debe5eeab103b4ddf1d6b28ace98d373a3a6
Monte Mishkin <montemishkin@gmail.com>
2015-07-19 14:42:16 -0700

    possibly fixed npm registry security issue

--- bash/bash_profile.sh
+++ bash/bash_profile.sh
@@ -1,3 +1,7 @@
+# secret environment variables
+source "$HOME/.secret.monte"
+
+
 # path to config repository
 repo_path="$HOME/mdv/config"
 
@@ -7,3 +11,6 @@ source "$repo_path/bash/config/aliases.sh"
 source "$repo_path/bash/config/setopts.sh"
 source "$repo_path/bash/config/prompt.sh"
 source "$repo_path/bash/config/completion.sh"
+
+
+# end of file
